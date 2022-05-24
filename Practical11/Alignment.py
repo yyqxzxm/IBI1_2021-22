7b8f31bc3fb25d8420e98cdb02795dbd24891d03
yyqxzxm <101240518+yyqxzxm@users.noreply.github.com>
2022-05-24 11:32:15 +0800

    push practical11

---
+++ Practical11/Alignment.py
@@ -0,0 +1,78 @@
+#This practical is difficult for me. I consulted a lot of information on the Internet and sought the help of senior students and Zuo Jingyi
+#read the document
+random=open('RandomSeq.fa')
+human=open('DLX5_human.fa')
+mouse=open('DLX5_mouse.fa')
+random_sequence=random.readlines()
+human_sequence=human.readlines()
+mouse_sequence=mouse.readlines()
+human_name=human_sequence[0]
+mouse_name=mouse_sequence[0]
+random_name=random_sequence[0]
+#acquire the amino acid sequence
+random_seq = random_sequence[1]
+human_seq = human_sequence[1].strip()
+mouse_seq = mouse_sequence[1].strip()
+length=len(mouse_seq)
+#caculate the account taken by identical amino acid in all amino acids
+a=0
+def comparison(seq1,seq2,x):
+    global a
+    for i in range(length):
+        if seq1[i]==seq2[i]:
+            a+=1
+    percent=a/length
+    print('the percent of identical amino acids between',x,'is',percent)
+comparison(random_seq,human_seq,'random and human')
+a=0
+comparison(human_seq,mouse_seq,'human and mouse')
+a=0
+comparison(random_seq,mouse_seq,'random and mouse')
+            
+    
+amino_acid_list=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','B','Z','X','*']
+BLOSUM62 = [
+            [ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+            [-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+            [-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+            [-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+            [-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+            [-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+            [-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+            [-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+            [-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+            [-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+            [-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+            [-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+            [-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+            [ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+            [ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+            [-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+            [-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+            [ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+            [-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+            [-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+            [ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+            [-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+#define the functions to align two amino acid sequence and acquire the score
+def amino_acid(amino_acid):
+    index = amino_acid_list.index(amino_acid)
+    return index
+def score(sequence1, sequence2):
+    score=0
+    for item in range(length):
+        index1=amino_acid(sequence1[item])
+        index2=amino_acid(sequence2[item])
+        one_score=BLOSUM62[index1][index2]
+        score+=one_score
+    return score
+x=score(mouse_seq,human_seq)
+y=score(mouse_seq,random_seq)
+z=score(human_seq,random_seq)
+print('\n')
+print('The score between mouse and human is:',x)
+print('The score between mouse and random is:',y)
+print('The score between random and human is:',z)
